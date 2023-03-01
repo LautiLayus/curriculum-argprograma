b97235f9ac43dc4c33ec9d1f11ab630f7c826ba5
Lauti Layus <84522759+LautiLayus@users.noreply.github.com>
2023-02-28 21:16:58 -0300

    Add files via upload
    archivos de curriruclum 1

---
+++ app.js
@@ -0,0 +1,27 @@
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function () {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    const className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+    
+    console.log('current class name: ' + className);
+});
+
+// Añade un efecto de desplazamiento suave a los enlaces internos
+$('a[href^="#"]').on('click', function(event) {
+  var target = $(this.getAttribute('href'));
+  if( target.length ) {
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: target.offset().top
+    }, 1000);
+  }
+});
